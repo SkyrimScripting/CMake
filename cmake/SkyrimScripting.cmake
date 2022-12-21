0989fc958c301d66b2b55f3337b17056b3094f20
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-21 07:23:37 -0800

    Debugging

--- cmake/SkyrimScripting.cmake
+++ cmake/SkyrimScripting.cmake
@@ -59,7 +59,7 @@ function(target_skyrim_precompile_headers TARGET)
     set(multiValueArgs)
     cmake_parse_arguments(PARSE_ARGV 1 SKYRIM_SCRIPTING_PRECOMPILE_HEADERS "${options}" "${oneValueArgs}" "${multiValueArgs}")
 
-    find_file(pch_header "Skyrim_SKSE_PCH.h")
+    find_file(pch_header "SkyrimScripting.CMake/Skyrim_SKSE_PCH.h")
     if(NOT EXISTS "${pch_header}")
         message(FATAL_ERROR "target_skyrim_precompile_headers error: Skyrim_SKSE_PCH.h not found via find_file")
     endif()
@@ -78,30 +78,12 @@ endfunction()
 # `add_skse_plugin` wraps calls to `add_commonlibsse_plugin` along with configuration for mod folder deployment et al.
 function(add_skse_plugin TARGET)
     set(options)
-    set(oneValueArgs DESTINATION MODS_FOLDER NAME AUTHOR EMAIL VERSION)
-    set(multiValueArgs SOURCES)
+    set(oneValueArgs DESTINATION MODS_FOLDER)
+    set(multiValueArgs)
     cmake_parse_arguments(PARSE_ARGV 1 SKYRIM_SCRIPTING_SKSE_PLUGIN "${options}" "${oneValueArgs}" "${multiValueArgs}")
 
-    # NAME AUTHOR EMAIL VERSION are all directly supported (and SOURCES) so they don't have to come last (with ${ARGN})
-    set(plugin_name ${TARGET})
-    if(DEFINED SKYRIM_SCRIPTING_SKSE_PLUGIN_NAME)
-        set(plugin_name "${SKYRIM_SCRIPTING_SKSE_PLUGIN_NAME}")
-    endif()
-    set(plugin_version ${PROJECT_VERSION})
-    if(DEFINED SKYRIM_SCRIPTING_SKSE_PLUGIN_VERSION)
-        set(plugin_version "${SKYRIM_SCRIPTING_SKSE_PLUGIN_VERSION}")
-    endif()
-
     find_package(CommonLibSSE CONFIG REQUIRED)
-    add_commonlibsse_plugin(
-        ${TARGET}
-        NAME "${plugin_name}"
-        VERSION "${project_vesion}"
-        AUTHOR "${SKYRIM_SCRIPTING_SKSE_PLUGIN_AUTHOR}"
-        EMAIL "${SKYRIM_SCRIPTING_SKSE_PLUGIN_EMAIL}"
-        SOURCES ${SKYRIM_SCRIPTING_SKSE_PLUGIN_SOURCES}
-        ${ARGN}
-    )
+    add_commonlibsse_plugin(${TARGET} ${SKYRIM_SCRIPTING_SKSE_PLUGIN_UNPARSED_ARGUMENTS})
     target_compile_features(${TARGET} PRIVATE cxx_std_23)
     target_skyrim_precompile_headers(${TARGET})
 
@@ -111,7 +93,6 @@ function(add_skse_plugin TARGET)
     elseif(DEFINED SKYRIM_SCRIPTING_SKSE_PLUGIN_MODS_FOLDER)
         set(mods_folder "${SKYRIM_SCRIPTING_SKSE_PLUGIN_MODS_FOLDER}")
     endif()
-
     if(DEFINED mods_folder)
         target_skyrim_mods_folder(${TARGET} DESTINATION "${mods_folder}")
     else()
