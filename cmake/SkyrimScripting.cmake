594a64d5ea2c50b50beea47873ae932e8f60833c
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-21 06:52:32 -0800

    Use target_skyrim_* as convention

--- cmake/SkyrimScripting.cmake
+++ cmake/SkyrimScripting.cmake
@@ -1,36 +1,36 @@
 # Provide a folder to auto-deploy your mod to after build
 #
 # ```
-# target_deploy_skyrim_mod(mytarget DESTINATION "C:/path/to/mods")
+# target_skyrim_mods_folder(mytarget DESTINATION "C:/path/to/mods")
 # ```
 #
 # Defaults to the `CMAKE_SKYRIM_MODS` environment variable, if set.
-function(target_deploy_skyrim_mod TARGET)
+function(target_skyrim_mods_folder TARGET)
     set(options)
     set(oneValueArgs DESTINATION)
     set(multiValueArgs)
-    cmake_parse_arguments(PARSE_ARGV 1 SKYRIM_SCRIPTING_DEPLOY_MOD "${options}" "${oneValueArgs}" "${multiValueArgs}")
+    cmake_parse_arguments(PARSE_ARGV 1 SKYRIM_SCRIPTING_MODS_FOLDER "${options}" "${oneValueArgs}" "${multiValueArgs}")
 
     if(NOT TARGET "${TARGET}")
-        message(FATAL_ERROR "Invalid target_deploy_skyrim_mod invocation. Target is not valid target: '${TARGET}'")
+        message(FATAL_ERROR "Invalid target_skyrim_mods_folder invocation. Target is not valid target: '${TARGET}'")
     endif()
 
-    if(DEFINED SKYRIM_SCRIPTING_DEPLOY_MOD_DESTINATION)
-        message("DEST: ${SKYRIM_SCRIPTING_DEPLOY_MOD_DESTINATION}")
-        if(IS_DIRECTORY "${SKYRIM_SCRIPTING_DEPLOY_MOD_DESTINATION}")
-            set(mods_folder "${SKYRIM_SCRIPTING_DEPLOY_MOD_DESTINATION}")
+    if(DEFINED SKYRIM_SCRIPTING_MODS_FOLDER_DESTINATION)
+        message("DEST: ${SKYRIM_SCRIPTING_MODS_FOLDER_DESTINATION}")
+        if(IS_DIRECTORY "${SKYRIM_SCRIPTING_MODS_FOLDER_DESTINATION}")
+            set(mods_folder "${SKYRIM_SCRIPTING_MODS_FOLDER_DESTINATION}")
         else()
-            message(FATAL_ERROR "Invalid target_deploy_skyrim_mod DESTINATION provided (directory does not exist) '${SKYRIM_SCRIPTING_DEPLOY_MOD_DESTINATION}'")
+            message(FATAL_ERROR "Invalid target_skyrim_mods_folder DESTINATION provided (directory does not exist) '${SKYRIM_SCRIPTING_MODS_FOLDER_DESTINATION}'")
         endif()
     else()
         if(DEFINED ENV{CMAKE_SKYRIM_MODS})
             if(IS_DIRECTORY "$ENV{CMAKE_SKYRIM_MODS}")
                 set(mods_folder "$ENV{CMAKE_SKYRIM_MODS}")
             else()
-                message(FATAL_ERROR "Invalid target_deploy_skyrim_mod invocation. CMAKE_SKYRIM_MODS environment variable (directory does not exist) '$ENV{CMAKE_SKYRIM_MODS}'")
+                message(FATAL_ERROR "Invalid target_skyrim_mods_folder invocation. CMAKE_SKYRIM_MODS environment variable (directory does not exist) '$ENV{CMAKE_SKYRIM_MODS}'")
             endif()
         else()
-            message(FATAL_ERROR "Invalid target_deploy_skyrim_mod invocation. No DESTINATION provided and no CMAKE_SKYRIM_MODS environment variable defined.")
+            message(FATAL_ERROR "Invalid target_skyrim_mods_folder invocation. No DESTINATION provided and no CMAKE_SKYRIM_MODS environment variable defined.")
         endif()
     endif()
 
